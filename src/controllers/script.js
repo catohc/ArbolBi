847c9b4326093b55944d82eb113be1ccd0c75cf1
Cato <catohjmz08@gmail.com>
2024-05-27 07:47:48 -0600

    firstCommit

--- src/controllers/script.js
+++ src/controllers/script.js
@@ -1,18 +1,41 @@
-import Contact from "../models/Contact.js"
+import Contact from "../models/Students.js"
+import BST from "../models/tree/BST.js"
 import { bst } from "./dependencies.js"
 
 let btn = document.getElementById("agenda-btn")
 
 btn.addEventListener("click",()=>{
     let firstName = document.getElementById("firstName").value
-    let lastName = document.getElementById("lastName").value
-    let phoneNumber = document.getElementById("phoneNumber").value
+    let matricula = document.getElementById("lastName").value
+    let old = document.getElementById("phoneNumber").value
 
-    let contacto = new Contact(firstName, lastName, phoneNumber)
-    let data = bst.add(contacto)
+    let student = new Contact(firstName, matricula, old)
+    let data = bst.add(student)
     console.log(data);
     if (data)
         alert("Registro exitoso")
     else
         alert("Falló el registro")
 })
+let btns = document.getElementById("Bagenda-btn");
+btns.addEventListener("click",()=>{
+    let smatricula = document.getElementById("lastName").value
+    let sdata = bst.search(smatricula);
+    console.log(sdata)
+    if (sdata) {
+        alert("yei")
+    }
+    else alert("au")
+});
+
+tree.preOrderTraversal((value) => {
+    alert(`Matricula: ${value.matricula}, Nombre: ${value.nombre}`);
+});
+
+let btnOrder = document.getElementById("WanderTree")
+btnOrder.addEventListener("click",()=>{
+let tree = new BST();
+tree.preOrderTraversal((value =>{
+    alert(`Matricula: ${value.matricula}, Nombre: ${value.nombre}`);
+}));
+});
--- src/models/tree/BST.js
+++ src/models/tree/BST.js
@@ -17,7 +17,7 @@ class BST {
     }
 
     insertNode(node, value){
-        if (value.lastName < node.value.lastName){
+        if (value.matricula < node.value.matricula){
             if (node.left == null){
                 node.left = new Node(value)
                 if (node.left != null) return true
@@ -33,6 +33,48 @@ class BST {
                 return this.insertNode(node.right, value)
         }
     }
+    search(matricula){
+        return this.searchNode(this.#root,matricula)
+    }
+    searchNode(node,matricula){
+        if (node == null ||node.value.matricula === matricula) {
+            return node;
+        }
+        else if (matricula < node.value.left) {
+            this.searchNode(node.left,matricula)
+        }
+        else return this.searchNode(node.right,matricula)
+    }
+    preOrderTraversal(callback) {
+        this.preOrder(this.#root, callback);
+    }
+
+    preOrder(node, callback) {
+        if (node != null) {
+            callback(node.value);
+            this.preOrder(node.left, callback);
+            this.preOrder(node.right, callback);
+        }
+    }
+
+    wander() {
+        let result = [];
+        this.preOrderTraversal((value) => {
+            result.push(value);
+        });
+        return result;
+    }
+    /*wander(){
+        this.marchTree(this.#root, result);
+    }
+    marchTree(node){
+        if (node != null) {
+            alert(node.value)
+            this.marchTree(node.left, result); 
+            this.marchTree(node.right, result); 
+        }
+    }
+    */
 }
 
 export default BST
